f11a83ebf802fb149157d106db31ebbe34598f87
Christopher Guarnera <christopher@techelevator.com>
2022-05-17 12:05:56 -0400

    final lecture code

--- module-1/02_Variables_and_Datatypes/lecture-final/src/main/java/com/techelevator/Lecture.java
+++ module-1/02_Variables_and_Datatypes/lecture-final/src/main/java/com/techelevator/Lecture.java
@@ -48,92 +48,119 @@ public class Lecture {
 		6. Create a variable called pi and set it to 3.1416.
 		*/
 
-		//System.out.println(pi);
+		double pi = 3.1416;
+		System.out.println(pi);
 
 		/*
 		7. Create and set a variable that holds your name.
 		*/
 
+		String myName = "Evan";
+
 		/*
 		8. Create and set a variable that holds the number of buttons on your mouse.
 		*/
+		int numberOfButtons = 2;
 
 		/*
 		9. Create and set a variable that holds the percentage of battery left on
 		your phone.
 		*/
+		double batteryPercentage = 0.86;
 
 		/*
 		10. Create an int variable that holds the difference between 121 and 27.
 		*/
+		int difference = 121 - 27;
 
 		/*
 		11. Create a double that holds the addition of 12.3 and 32.1.
 		*/
+		double sum = 12.3 + 32.1;
 
 		/*
 		12. Create a String that holds your full name.
 		*/
+		String myFullName = "Dalton Kopyar";
 
 		/*
 		13. Create a String that holds the word "Hello, " concatenated onto your
 		name from above.
 		*/
+		String hello = "Hello, ";
+		String sentence = hello + myFullName;
+		System.out.println( sentence);
 
 		/*
 		14. Add a " Esquire" onto the end of your full name and save it back to
 		the same variable.
 		*/
+		myFullName = myFullName + " Esquire";
+
 
 		/*
 		15. Now do the same as exercise 14, but use the += operator.
 		*/
+		myFullName += " Esquire";
 
 		/*
 		16. Create a variable to hold "Saw" and add a 2 onto the end of it.
 		*/
+		String saw = "Saw";
+		String saw2 = saw + 2;
+		System.out.println(saw2);
 
 		/*
 		17. Add a 0 onto the end of the variable from exercise 16.
 		*/
+		System.out.println(saw2 + 0);
 
 		/*
 		18. What is 4.4 divided by 2.2?
 		*/
+		System.out.println(4.4/2.2);
 
 		/*
 		19. What is 5.4 divided by 2?
 		*/
+		System.out.println(5.4/2);
 
 		/*
 		20. What is 5 divided by 2?
 		*/
+		System.out.println(((double)5)/2);
 
 		/*
 		21. What is 5.0 divided by 2?
 		*/
+		System.out.println(5.0/2);
 
 		/*
 		22. What is 66.6 divided by 100? Is the answer you get right or wrong?
 		*/
+		System.out.println(66.6/100);
 
 		/*
 		23. If I divide 5 by 2, what's my remainder?
 		*/
+		System.out.println(5 % 2);
 
 		/*
 		24. What is 1,000,000,000 * 3?
 		*/
+		long billions = ((long)1_000_000_000) * 3;
+		System.out.println(billions);
 
 		/*
 		25. Create a variable that holds a boolean called isDoneWithExercises and
 		set it to false.
 		*/
+		boolean isDoneWithExercises = false;
 
 		/*
 		26. Now set isDoneWithExercise to true.
 		*/
-		
+		isDoneWithExercises = true;
 	}
 
 }
