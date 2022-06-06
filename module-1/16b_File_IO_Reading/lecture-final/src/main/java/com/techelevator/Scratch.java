0e22b82e2b978e05b8f022f863ded20f7062c6db
Christopher Guarnera <christopher@techelevator.com>
2022-06-06 11:39:27 -0400

    lecture-final notes

--- module-1/16b_File_IO_Reading/lecture-final/src/main/java/com/techelevator/Scratch.java
+++ module-1/16b_File_IO_Reading/lecture-final/src/main/java/com/techelevator/Scratch.java
@@ -1,13 +1,37 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import com.techelevator.exceptions.OrangeException;
 
 public class Scratch {
 
 
     public static void main(String[] args) {
+
+
+        File myFile = new File("C:\\Users\\Student\\instructor_repos\\instructor-code\\module-1\\16b_File_IO_Reading\\lecture-final\\rtn.txt");
+
+        // Declaring the scanner in curvies after the try will automatically open and close for you, so you don't have to worry about a finally
+        try(Scanner myFileReader = new Scanner(myFile)) {
+
+            while(myFileReader.hasNextLine()) {
+                String myLine = myFileReader.nextLine();
+                System.out.println(myLine);
+            }
+
+        } catch( FileNotFoundException ex) {
+            System.out.println("File was not found, check the path");
+        } catch(Exception ex) {
+            System.out.println("Something went wrong");
+        }
+
+        /*
+        Scratch s = new Scratch();
+        s.method1();
 
         boolean validInput = false;
         while(!validInput) {
@@ -18,8 +42,8 @@ public class Scratch {
 
                 int dogAgeInHumanYears = Integer.parseInt(ageInput) * 7;
 
-                String myName = null;
-                System.out.println("My name is length " + myName.length());
+               // String myName = null;
+               // System.out.println("My name is length " + myName.length());
 
                 System.out.println("Your dog is " + dogAgeInHumanYears + " in human years");
 
@@ -32,8 +56,55 @@ public class Scratch {
                 System.out.println("Your name cannot be null");
             } catch (Exception ex) {
                 System.out.println("Something went wrong");
+            } finally {
+                // this code will run regardless of if an exception was thrown
+                // we can clean up any open connections - for instance to a database
+                System.out.println("This is the finally");
             }
+
+
         }
+*/
+
+
+    }
+
+    public void run() {
+        try{
+            checkClass("orange");
+        } catch(OrangeException ex) {
+            System.out.println("THis is a custom exception and we have a custom field I can access, numStudents:" + ex.getNumStudents());
+        }
+    }
+
+    /*Example to demonstrate using custom exception */
+    public String checkClass(String color) throws OrangeException {
+
+        if(color.equals("orange")) {
+            throw new OrangeException("We are the exception", 21);
+        }
+
+        return color;
+
+    }
+
+    public void method1() {
+        try {
+            method2();
+        } catch(NullPointerException ex) {
+            System.out.println("Null Pointer!");
+
+        } catch(Exception ex) {
+            System.out.println("There was an error");
+        }
+    }
+
+    public void method2() throws Exception{
+        method3();
+    }
+
+    public void method3() throws Exception{
+        throw new Exception("null!");
 
     }
 
