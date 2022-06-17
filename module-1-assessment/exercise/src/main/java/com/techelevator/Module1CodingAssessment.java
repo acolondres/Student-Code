340f73ab897fd1f720dc9bd2a357d9817db133b8
Christopher Guarnera <christopher@techelevator.com>
2022-06-17 08:37:03 -0400

    module 1 assessment

---
+++ module-1-assessment/exercise/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+import java.time.LocalDateTime;
+
+public class Module1CodingAssessment {
+
+	public static void main(String[] args) {
+
+		// How to retrieve the current year
+		int currentYear = LocalDateTime.now().getYear();
+		System.out.println(currentYear);
+
+	}
+
+}
