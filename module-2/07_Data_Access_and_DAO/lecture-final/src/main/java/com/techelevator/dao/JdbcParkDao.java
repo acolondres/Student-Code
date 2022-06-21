dabeaaa6c49b8d0582bfabd63a1f2ac4a48c3346
Christopher Guarnera <christopher@techelevator.com>
2022-06-21 11:58:28 -0400

    lecture final initial park dao

--- module-2/07_Data_Access_and_DAO/lecture-final/src/main/java/com/techelevator/dao/JdbcParkDao.java
+++ module-2/07_Data_Access_and_DAO/lecture-final/src/main/java/com/techelevator/dao/JdbcParkDao.java
@@ -11,14 +11,32 @@ import java.util.List;
 public class JdbcParkDao implements ParkDao {
 
     private final JdbcTemplate jdbcTemplate;
-
     public JdbcParkDao(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
     public Park getPark(int parkId) {
-        return new Park();
+
+        // Step 1
+        Park park = null;
+
+
+        //Step 2
+        String sql = "SELECT park_id, park_name, date_established, area, has_camping " +
+                "FROM park " +
+                "WHERE park_id = ? ;";
+
+        //Step 3
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, parkId);
+
+        // Step 4
+        if(results.next()) {
+            park = mapRowToPark(results);
+        }
+
+        // Step 5
+        return park;
     }
 
     @Override
@@ -39,6 +57,7 @@ public class JdbcParkDao implements ParkDao {
     @Override
     public void deletePark(int parkId) {
 
+
     }
 
     @Override
@@ -52,6 +71,19 @@ public class JdbcParkDao implements ParkDao {
     }
 
     private Park mapRowToPark(SqlRowSet rowSet) {
-        return new Park();
+        Park park = new Park();
+
+        // all the transfers down here
+        park.setParkId(rowSet.getInt("park_id"));
+        park.setParkName(rowSet.getString("park_name"));
+
+        if(rowSet.getDate("date_established") != null) {
+            park.setDateEstablished(rowSet.getDate("date_established").toLocalDate());
+        }
+
+        park.setArea(rowSet.getDouble("area"));
+        park.setHasCamping(rowSet.getBoolean("has_camping"));
+
+        return park;
     }
 }
