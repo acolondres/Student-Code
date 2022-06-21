ef55b9615ed51a9f36aeee393a186717ff6db948
Andy Colondres <andyncolondres@gmail.com>
2022-06-21 08:06:35 -0400

    udate delete postgres HW

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/12_add_collection.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/12_add_collection.sql
@@ -1,2 +1,11 @@
 -- 12. Create a "Bill Murray Collection" in the collection table. For the movies that have Bill Murray in them, set their collection ID to the "Bill Murray Collection". (1 row, 6 rows)
 
+ INSERT INTO collection(collection_name)
+ VALUES ('Bill Murray Collection');
+
+
+ UPDATE movie
+ SET collection_id = (SELECT collection_id FROM collection WHERE collection_name = 'Bill Murray Collection')
+ WHERE movie.movie_id in (SELECT movie.movie_id FROM movie JOIN movie_actor ON movie.movie_id = movie_actor.movie_id
+                                                            JOIN person ON movie_actor.actor_id = person.person_id
+                                                            WHERE person_name= 'Bill Murray');
