1b024b34d781370dae8a00692e7ab954d17d837a
Christopher Guarnera <christopher@techelevator.com>
2022-07-28 11:55:49 -0400

    lecture final and exercise

---
+++ module-3/13_Vue_Component_Communication/cg-lecture-final/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+   <my-example v-bind:name="'Christopher'"
+              v-bind:dayOfWeekProp="dayOfWeekValue"></my-example>
+<product-review></product-review>
+  <input type="date">
+  
+  </div>
+</template>
+
+<script>
+import MyExample from "./components/MyExample.vue"
+import ProductReview from "./components/ProductReview.vue"
+
+export default {
+  name: "app",
+  components: {
+    MyExample, ProductReview
+  },
+  data() {
+    return {
+     
+    }
+  },
+  computed: {
+    dayOfWeekValue(){
+    const weekday = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      const d = new Date();
+      let day = weekday[d.getDay()];
+      return day;
+
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+</style>
---
+++ module-3/13_Vue_Component_Communication/cg-lecture-final/src/components/AddStudent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <label>First Name</label>
+        <input type="text" v-model="firstName" />
+        <label>Last Name</label>
+        <input type="text" v-model="lastName" />
+        <button v-on:click="addStudent">Add Student </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "add-student",
+    data() {
+        return {
+            firstName: "",
+            lastName: ""
+        }
+    },
+    methods: {
+        addStudent(){
+
+           let student = {firstName: this.firstName,
+                          lastName: this.lastName};
+           this.$store.commit("ADD_STUDENT", student);
+            this.firstName = "";
+            this.lastName = "";
+       }
+    }
+}
+</script>
