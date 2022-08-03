2224fe66bc75d8fd53f1af5b75f8251568593002
Christopher Guarnera <christopher@techelevator.com>
2022-08-03 11:53:45 -0400

    lecture -final and exercise

---
+++ module-3/16_Web_Services_GET/cg-lecture-final/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <astronomy >
+    </astronomy>
+  </div>
+</template>
+
+<script>
+import Astronomy from '@/components/Astronomy.vue'
+
+export default {
+  components: {
+    Astronomy
+  }
+};
+</script>
